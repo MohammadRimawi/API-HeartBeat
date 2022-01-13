4d4a0dbf4b09c2ff4e9dc2d7ee64b1d3d34b7e33
MohammadRimawi <mohrimawiz@gmail.com>
2022-01-13 12:16:09 +0200

    Creating DB, and it's insert and query function

--- app/src/main/java/com/devclass/apiheartbeat/MainActivity.kt
+++ app/src/main/java/com/devclass/apiheartbeat/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.devclass.apiheartbeat
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,6 +11,7 @@ import java.net.HttpURLConnection
 import java.net.URL
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.view.View
 import okhttp3.ResponseBody
 import retrofit2.*
 import retrofit2.http.GET
@@ -21,6 +25,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // create a list of server objects, specifying all required attributes to every server.
+
+        fun addServer(){
+            val values = ContentValues()
+            values.put(SQLite.NAME, "Varla");
+            values.put(SQLite.URL, "rimawi.me");
+            values.put(SQLite.SCEHMA, "http");
+            values.put(SQLite.PORT, "5050");
+            values.put(SQLite.ENDPOINT, "/api/get/pinned_todos");
+            values.put(SQLite.METHOD, "post");
+
+            val uri = contentResolver.insert(
+                SQLite.CONTENT_URL, values
+            )
+        }
+
+        @SuppressLint("Range")
+        fun retrieveServers(){
+
+            val URL = "content://com.devclass.apiheartbeat.SQLite";
+            val servers = Uri.parse(URL)
+            var SQLiteResolver = contentResolver.query(servers, null, null, null,null)
+
+            if(SQLiteResolver!=null){
+                if(SQLiteResolver?.moveToFirst()){
+                    do{
+                        println("*******************************")
+                        println(SQLiteResolver.getString(SQLiteResolver.getColumnIndex(SQLite.NAME)))
+                    }while (SQLiteResolver?.moveToNext());
+                }
+            }
+//            return arrayOf<Server>(Server(name = "My CV", schema = "http",url = "rimawi.me",method = "GET"));
+        }
+
+        retrieveServers()
 
         var servers = arrayOf<Server>(
                 Server(name = "My CV", schema = "http",url = "rimawi.me",method = "GET"),
--- app/src/main/java/com/devclass/apiheartbeat/SQLite.kt
+++ app/src/main/java/com/devclass/apiheartbeat/SQLite.kt
@@ -1,35 +1,70 @@
 package com.devclass.apiheartbeat
 
-import android.content.ContentProvider
-import android.content.ContentValues
-import android.content.UriMatcher
+import android.content.*
 import android.database.Cursor
+import android.database.SQLException
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.database.sqlite.SQLiteQueryBuilder
 import android.net.Uri
 import java.util.HashMap
 
 class SQLite:ContentProvider() {
     companion object{
         val PROVIDER_NAME = "com.devclass.apiheartbeat.SQLite";
-        val URL = "content://"+ PROVIDER_NAME + "/servers";
-        val CONTENT_URL = Uri.parse(URL);
+        val prov_URL = "content://"+ PROVIDER_NAME + "/servers";
+        val CONTENT_URL = Uri.parse(prov_URL);
+
         val _ID = "_id";
         val NAME = "name";
-        val GRADE = "grade";
+        val METHOD = "method";
+        val SCEHMA = "schema";
+        val URL = "url";
+        val PORT = "port";
+        val ENDPOINT = "endpoint";
+
         private val SERVER_PROJECTION_MAP: HashMap<String, String>? = null;
-        val STUDENTS = 1;
-        val STUDENT_ID = 2;
+
+        val SERVERS = 1;
+        val SERVER_ID = 2;
+
         val uriMatcher : UriMatcher? = null;
-        val DATABASE_NAME = "College";
-        val STUDENT_TABLE_NAME = "students";
-        val DATABSE_VERSION = 1;
+        val DATABASE_NAME = "heartbeat";
+        val SERVERS_TABLE_NAME = "servers";
+        val DATABASE_VERSION = 1;
+        val CREATE_DB_TABLE = "CREATE TABLE " + SERVERS_TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT,method TEXT NOT NULL,name TEXT NOT NULL,schema TEXT NOT NULL,url TEXT NOT NULL,port TEXT ,endpoint TEXT );";
 
-        val CREATE_DB_TABLE = "CREATE TABLE " + STUDENT_TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, grade TEXT NOT NULL);"
+        private var sUriMatcher = UriMatcher(UriMatcher.NO_MATCH);
 
+        init{
+            sUriMatcher.addURI(PROVIDER_NAME, "servers", SERVERS);
+            sUriMatcher.addURI(PROVIDER_NAME, "server_id/#", SERVER_ID);
+        }
+
+
+        private var db: SQLiteDatabase? = null
+
+        private class DatabaseHelper internal constructor(context: Context?) :
+            SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+                override fun onCreate(db: SQLiteDatabase) {
+                    db.execSQL(CREATE_DB_TABLE);
+                }
+
+                override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+                    db.execSQL("DROP TABLE IF EXISTS " + SERVERS_TABLE_NAME);
+                    onCreate(db);
+                }
+        }
     }
 
     override fun onCreate(): Boolean {
-        TODO("Not yet implemented")
+
+        val context = context;
+        val dbHelper = DatabaseHelper(context)
+        db = dbHelper.writableDatabase;
+
+        return db!=null;
+
     }
 
     override fun query(
@@ -39,7 +74,20 @@ class SQLite:ContentProvider() {
         p3: Array<out String>?,
         p4: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        println("####################### IN QUERY")
+        val qb = SQLiteQueryBuilder();
+        qb.tables = SERVERS_TABLE_NAME;
+
+//        when (uriMatcher!!.match(p0)) {
+//            SERVER_ID -> qb.appendWhere(_ID + "=" + p0.pathSegments[1])
+//            else -> {
+//                null
+//            }
+//        }
+        val sQLiteResolver = qb.query(db,null,null,null,null,null,null);
+        sQLiteResolver.setNotificationUri(context!!.contentResolver, p0)
+
+        return sQLiteResolver
     }
 
     override fun getType(p0: Uri): String? {
@@ -47,7 +95,13 @@ class SQLite:ContentProvider() {
     }
 
     override fun insert(p0: Uri, p1: ContentValues?): Uri? {
-        TODO("Not yet implemented")
+        val last_row_id = db!!.insert(SERVERS_TABLE_NAME,"",p1);
+        if(last_row_id>0){
+            val _uri = ContentUris.withAppendedId(CONTENT_URL,last_row_id);
+            context!!.contentResolver.notifyChange(_uri,null);
+            return _uri;
+        }
+        throw SQLException("Failed to add record into ${p0}");
     }
 
     override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
@@ -57,6 +111,8 @@ class SQLite:ContentProvider() {
     override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
         TODO("Not yet implemented")
     }
+
+
 
 }
 
